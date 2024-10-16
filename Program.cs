ac4fa80959674c793e9d0f2e341864897c52156e
hana-ben-amor <hanabenamor2001@gmail.com>
2024-10-16 13:10:03 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    //static void Main(string[] args)
+    //{
+    //    int age = 0;
+    //    Console.WriteLine("Quel âge avez-vous?");
+
+    //    try
+    //    {
+           
+    //        age = int.Parse(Console.ReadLine());
+    //        Console.WriteLine("Vous avez " + age + " ans !");
+    //    }
+    //    catch (FormatException ex)
+    //    {
+    //        Console.WriteLine("Erreur : Vous devez entrer un nombre entier valide !");
+    //    }
+    //    catch (OverflowException ex)
+    //    {
+           
+    //        Console.WriteLine("Erreur : Le nombre entré est trop grand ou trop petit.");
+    //    }
+    //    finally
+    //    {
+        
+    //        Console.WriteLine("Merci d'avoir utilisé notre programme.");
+    //    }
+
+    //    Console.ReadLine();
+    //}
+}
---
+++ ex2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    internal class ex2
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nous allons faire une division avec des nombres positifs :");
+
+            try
+            {
+                Console.Write("\nEntrez un premier nombre : ");
+                uint a = uint.Parse(Console.ReadLine());
+
+                Console.Write("Entrez un deuxieme nombre : ");
+                uint b = uint.Parse(Console.ReadLine());
+
+                // Check for division by zero
+                if (b == 0)
+                {
+                    throw new DivideByZeroException(); // Manually throw exception for division by zero
+                }
+
+                uint c = a / b;
+                Console.WriteLine("Le résultat de la division est " + c);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Erreur : Vous devez entrer un nombre entier positif !");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Erreur : Le nombre entré est trop grand ou trop petit.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Erreur : Division par zéro n'est pas autorisée.");
+            }
+            catch (Exception ex)
+            {
+                // This will catch any other exceptions that were not handled above
+                Console.WriteLine("Une erreur inattendue est survenue : " + ex.Message);
+            }
+            finally
+            {
+                // Code that will always run, regardless of exceptions
+                Console.WriteLine("Merci d'avoir utilisé notre programme.");
+            }
+
+            Console.ReadLine();
+        }
+    
+
+}
+}
